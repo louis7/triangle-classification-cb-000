82de95ffdac4f513c4223073ed09f35e715d2673
louis baptiste <louisvis@ymail.com>
2018-08-07 23:46:41 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,50 @@
+require "pry"
 class Triangle
-  # write code here
+attr_accessor :equilateral, :isosceles,:scalene
+
+
+def initialize(length_1, length_2, length_3)
+  @length_1 = length_1
+  @length_2 = length_2
+  @length_3 = length_3
+  @total = []
+  @total<< @length_1 << @length_2 << @length_3
+
+end
+
+
+
+def kind
+sanity
+  if @length_1 == @length_2 && @length_1 == @length_3
+    return :equilateral
+
+
+  elsif @length_1 == @length_2 || @length_1 == @length_3 || @length_2 == @length_3
+      return :isosceles
+
+else
+  return :scalene
+  end
+
+end
+
+
+ def sanity
+  @total.each do |items|
+      if items <= 0
+        raise TriangleError
+      end
+  end
+if @total[0] +@total[1] < @total[2]
+  binding.pry 
+  raise TriangleError
+end
+
+end
+
+
+class TriangleError < StandardError
+end
+
 end
